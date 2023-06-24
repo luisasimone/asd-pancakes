a29612a93e22208aa323dac3d4ccc00f47b20f78
Luisa Simone <luisasimone1997@gmail.com>
2023-06-24 23:48:53 +0200

    1st and 2nd requests done

--- exam_220209/cmake-build-debug/cmake_install.cmake
+++ exam_220209/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/luiza/CLionProjects/2022/asd-pancakes/exam_220209
+# Install script for directory: /home/luiza/CLionProjects/asd-pancakes/exam_220209
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/luiza/CLionProjects/2022/asd-pancakes/exam_220209/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/luiza/CLionProjects/asd-pancakes/exam_220209/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
